dafdff2edf052c912f6480170cc6cf0f134d8aac
Nurlan <nurlan.emiroff@gmail.com>
2017-08-22 16:00:03 +0200

    WIP Account class - added expiration date

--- lib/account.rb
+++ lib/account.rb
@@ -1,13 +1,21 @@
+require 'date'
+
 class Account
-attr_accessor :pin_code, :balance
+  STANDARD_VALIDITY_YRS = 5
+attr_accessor :pin_code, :balance, :exp_date
 
   def initialize
     @pin_code = generate_pin
     @balance = 0
+    @exp_date = set_expire_date
 end
 
 private
 def generate_pin
   rand(1000..9999)
+end
+
+def set_expire_date
+  Date.today.next_year(5).strftime('%m/%Y')
 end
   end
