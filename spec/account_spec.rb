6b1063d3a6aca617177f73b75b6aa13afb526f8d
Nurlan <nurlan.emiroff@gmail.com>
2017-08-22 15:31:08 +0200

    WIP Account class - added balance to account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,4 +6,8 @@ it 'is expected to have a 4 digit pin number in initialize' do
   pin_length = Math.log10(subject.pin_code).to_i + 1
   expect(pin_length).to eq 4
   end
+
+  it 'is expected to have a balance of 0 on initialize' do
+  expect(subject.balance).to eq 0
+  end
 end
