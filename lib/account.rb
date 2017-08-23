4c39bfff09666e0f733a2e3ac8cc84c84faf7dd1
Nurlan <nurlan.emiroff@gmail.com>
2017-08-23 13:38:35 +0200

    The account

--- lib/account.rb
+++ lib/account.rb
@@ -2,13 +2,15 @@ require 'date'
 
 class Account
   STANDARD_VALIDITY_YRS = 5
-attr_accessor :pin_code, :balance, :exp_date
+attr_accessor :pin_code, :balance, :exp_date, :account_status, :owner
 
-  def initialize
+  def initialize(attrs = {})
     @pin_code = generate_pin
     @balance = 0
     @exp_date = set_expire_date
-end
+    @account_status = :active
+    @owner = set_owner(attrs[:owner])
+  end
 
 private
 def generate_pin
@@ -17,5 +19,17 @@ end
 
 def set_expire_date
   Date.today.next_year(5).strftime('%m/%Y')
+end
+
+def deactivate
+  @account_status = :deactivated
+end
+
+def set_owner(obj)
+  obj == nil ?  missing_owner : obj
+end
+
+def missing_owner
+  raise "An Account owner is required"
 end
   end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,17 +3,32 @@ require 'date'
 
 describe Account do
 
-it 'is expected to have a 4 digit pin number in initialize' do
-  pin_length = Math.log10(subject.pin_code).to_i + 1
-  expect(pin_length).to eq 4
+  let(:person) {instance_double('Person', name: 'Nurlan')}
+  subject { described_class.new({owner: person}) }
+
+  it 'is expected to have a 4 digit pin number in initialize' do
+    pin_length = Math.log10(subject.pin_code).to_i + 1
+    expect(pin_length).to eq 4
   end
 
   it 'is expected to have a balance of 0 on initialize' do
-  expect(subject.balance).to eq 0
-end
+    expect(subject.balance).to eq 0
+  end
 
   it 'is expected to have an expiry date on initialize' do
     expected_date = Date.today.next_year(5).strftime("%m/%Y")
     expect(subject.exp_date).to eq expected_date
+  end
+
+  it 'is expected to have :active status on initialize' do
+    expect(subject.account_status).to eq :active
+  end
+
+  it 'is expected to have an owner' do
+    expect(subject.owner).to eq person
+  end
+
+  it 'is expected to raise error if no owner is set' do
+    expect { described_class.new }.to raise_error 'An Account owner is required'
   end
 end
